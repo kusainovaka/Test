8af3993e1a281e9a5ae844466048d5a50a06f16a
kamila kussainova <kusainovaka@gmal.com>
2020-10-21 11:50:26 +0600

    TODO

--- Test/LargeTitle/LargeTitlePage.swift
+++ Test/LargeTitle/LargeTitlePage.swift
@@ -25,9 +25,15 @@ private struct Const {
     static let NavBarHeightLargeState: CGFloat = 96.5
 }
 
+enum NavBarState {
+    case hidden
+    case show
+    case none
+}
+
 class LargeTitlePage: UIViewController {
     
-    let tableView = UITableView(frame: .zero, style: .grouped)
+    let tableView = UITableView()
     let imageView: UIImageView = {
         let image = UIImageView()
         image.backgroundColor = .orange
@@ -42,24 +48,78 @@ class LargeTitlePage: UIViewController {
         view.backgroundColor = .red
         setupNavBar()
         setupTableView()
+//        title = "Платежи и переводы"
     }
     
     func setupNavBar() {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationBar.frame = CGRect(x: 0, y: 0, width: 375, height: 200)
         imageAnimation()
-        titleAnimation()
+        setupNavigationMultilineTitle()
+//        titleAnimation()
+        test()
     }
 
     func imageAnimation() {
         guard let navigationBar = self.navigationController?.navigationBar else { return }
         navigationBar.addSubview(imageView)
-        
         imageView.snp.makeConstraints { make in
             make.right.equalToSuperview().offset(-52)
             make.bottom.equalToSuperview().offset(-Const.ImageBottomMarginForLargeState)
             make.size.equalTo(Const.ImageSizeForLargeState)
         }
+    }
+    
+    func setTitle(hidden: Bool) {
+        let animation = CATransition()
+        animation.duration = 0.25
+        navigationController?.navigationBar.layer.add(animation, forKey: "fadeText")
+        titleLabel.isHidden = hidden
+        if hidden {
+            navigationItem.title = "Платежи и переводы"
+        } else {
+            navigationItem.title = ""
+            titleAnimation()
+        }
+    }
+    
+    
+    func setupNavigationMultilineTitle() {
+        print(navigationItem.titleView?.subviews, "HELLO")
+//        let label = UILabel()
+//        label.numberOfLines = 0
+//        label.text = "Платежи и переводы"
+//        label.lineBreakMode = .byWordWrapping
+//        label.textColor = .red
+//        navigationItem.titleView = label
+//        debugPrint(navigationItem.titleView)
+//        guard let navigationBar = self.navigationController?.navigationBar else { return }
+//        for sview in navigationBar.subviews {
+//            for ssview in sview.subviews {
+//                print(ssview, "SSVIEW")
+////                guard let label = ssview as? UILabel else { break }
+//                print("Goood")
+//                if label.text == self.title {
+//                    label.numberOfLines = 0
+//                    label.text = "Платежи и переводы"
+//                    label.lineBreakMode = .byWordWrapping
+//                    label.textColor = .red
+//                    label.sizeToFit()
+//                    UIView.animate(withDuration: 0.3, animations: {
+//                        navigationBar.frame.size.height = 57 + label.frame.height
+//                    })
+//                }
+//            }
+//        }
+
+    }
+    
+    func test() {
+        print(navigationController?.navigationBar.topItem?.titleView , "topItem")
+        print(navigationItem.titleView, "titleView")
+        print(navigationController?.navigationBar.subviews, "SubViews")
+        navigationController?.navigationBar.accessibilityLabel = "Testt"
+//        print(navigationController?.navigationBar.l)
     }
     
     func titleAnimation() {
@@ -107,26 +167,27 @@ class LargeTitlePage: UIViewController {
             .translatedBy(x: xTranslation, y: yTranslation)
         
         // TODO:
-        if coeff == 0 {
-            titleLabel.font = UIFont.boldSystemFont(ofSize: 17)
-            titleLabel.snp.removeConstraints()
-            titleLabel.snp.makeConstraints { make in
-                make.centerX.equalToSuperview()
-                make.bottom.equalToSuperview().offset(-18)
-            }
-            imageView.snp.updateConstraints { make in
-                make.bottom.equalToSuperview().offset(-14)
-            }
-            
-        } else {
-            titleLabel.font = UIFont.boldSystemFont(ofSize: 32)
-            titleLabel.snp.removeConstraints()
+        debugPrint(coeff, sizeDiff, "Test=======", xTranslation, factor)
+        if coeff < 0.5 {
+//            titleLabel.font = UIFont.boldSystemFont(ofSize: 17)
+//            titleLabel.snp.removeConstraints()
+//            titleLabel.snp.makeConstraints { make in
+//                make.centerX.equalToSuperview()
+//                make.bottom.equalToSuperview().offset(-18)
+//            }
+//            imageView.snp.updateConstraints { make in
+//                make.bottom.equalToSuperview().offset(-14)
+//            }
 
-            titleLabel.snp.makeConstraints { make in
-                make.left.equalToSuperview().offset(16)
-                make.right.equalTo(imageView.snp.left).offset(-60)
-                make.bottom.equalToSuperview()
-            }
+        } else {
+//            titleLabel.font = UIFont.boldSystemFont(ofSize: 32)
+//            titleLabel.snp.removeConstraints()
+//
+//            titleLabel.snp.makeConstraints { make in
+//                make.left.equalToSuperview().offset(16)
+//                make.right.equalTo(imageView.snp.left).offset(-60)
+//                make.bottom.equalToSuperview()
+//            }
         }
     }
     
@@ -138,39 +199,42 @@ class LargeTitlePage: UIViewController {
         tableView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
-//        setupTableHeaderView()
+        setupTableHeaderView()
     }
     
     func setupTableHeaderView() {
-        let headerView = UIView()
-        headerView.backgroundColor = .green
+        let segmentView = UIView()
+        segmentView.backgroundColor = .red
+        let filterView = UIView()
+        filterView.backgroundColor = .green
+        let headerView = UIStackView()
+        headerView.distribution = .fillProportionally
+        headerView.axis = .vertical
+        headerView.addArrangedSubview(segmentView)
+        headerView.addArrangedSubview(filterView)
         headerView.snp.makeConstraints { make in
             make.width.equalTo(375)
             make.height.equalTo(56)
         }
-        
+
         tableView.tableHeaderView = headerView
     }
 }
 
 extension LargeTitlePage: UITableViewDataSource, UITableViewDelegate {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let headerView = UIView()
-        headerView.backgroundColor = .blue
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return 1
+//    }
+//
+//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        let headerView = UIView()
+//        headerView.backgroundColor = .blue
 //        headerView.snp.makeConstraints { make in
-////            make.width.equalTo(375)
-//            make.height.equalTo(56)
+//            make.width.equalTo(375)
+//            make.height.equalTo(40)
 //        }
-        return headerView
-    }
-    
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 56
-    }
+//        return headerView
+//    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 30
@@ -183,7 +247,68 @@ extension LargeTitlePage: UITableViewDataSource, UITableViewDelegate {
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let myLabelHeight: CGFloat = 60
+//        if scrollView.contentOffset.y > myLabelHeight {
+//                setTitle(hidden: false)
+//            } else if scrollView.contentOffset.y <= myLabelHeight {
+//                setTitle(hidden: true)
+//            }
         guard let height = navigationController?.navigationBar.frame.height else { return }
+//        print("HEight", height, "YYY", scrollView.contentOffset.y)
+//        print(height, myLabelHeight, "👻👻👻👻👻👻👻")
+//        if height > myLabelHeight {
+//            print("11111")
+//            setupTitle(type: .show)
+////            setTitle(hidden: false)
+//        } else if height < 80 && 80 < 60 {
+//            print("22222")
+//        } else if height <= myLabelHeight {
+////            setTitle(hidden: true)
+//            print("3333")
+//            setupTitle(type: .hidden)
+//        }
+        
+        
+//        switch height {
+//            case 96 ... 80:
+//            print("1111")
+//            case 80 ... 60:
+//            print("222222")
+//            case 60 ... 40:
+//            print("3333")
+//            default:
+//            print("finish")
+//        }
+        
+        print(height, "HEight")
+        if height > 80 {
+            setupTitle(type: .show)
+//            print("1111")
+        } else if height < 74 {
+            if height < 56 {
+                setupTitle(type: .hidden)
+//                print("3333", height)
+            } else {
+                setupTitle(type: .none)
+//                print("22222")
+            }
+        }
+        
         moveAndResizeImage(for: height)
+    }
+    
+    func setupTitle(type: NavBarState) {
+        switch type {
+            case .hidden:
+                navigationItem.title = "Платежи и переводы"
+                titleLabel.isHidden = true
+            case .show:
+                navigationItem.title = ""
+                titleAnimation()
+                titleLabel.isHidden = false
+            case .none:
+                navigationItem.title = ""
+                titleLabel.isHidden = true
+        }
     }
 }
